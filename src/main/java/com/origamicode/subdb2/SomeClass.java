0d813751fec3cbb635daf439766ab02966859900
Marek Pulka <marek.pulka@gmail.com>
2014-03-28 09:39:09 +0100

    1.7 code

--- src/main/java/com/origamicode/subdb2/SomeClass.java
+++ src/main/java/com/origamicode/subdb2/SomeClass.java
@@ -1,8 +1,35 @@
 package com.origamicode.subdb2;
 
 public class SomeClass {
-  
+
   public final int add(int param1, int param2) {
     return param1 + param2;
+  }
+
+  /*
+   * Code using 1.7 lang features
+   */
+  public final String getTypeOfDayWithSwitchStatement(String dayOfWeekArg) {
+    String typeOfDay;
+    switch (dayOfWeekArg) {
+      case "Monday":
+        typeOfDay = "Start of work week";
+        break;
+      case "Tuesday":
+      case "Wednesday":
+      case "Thursday":
+        typeOfDay = "Midweek";
+        break;
+      case "Friday":
+        typeOfDay = "End of work week";
+        break;
+      case "Saturday":
+      case "Sunday":
+        typeOfDay = "Weekend";
+        break;
+      default:
+        throw new IllegalArgumentException("Invalid day of the week: " + dayOfWeekArg);
+    }
+    return typeOfDay;
   }
 }
