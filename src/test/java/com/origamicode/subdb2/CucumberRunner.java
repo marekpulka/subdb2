e678a67a8fb3a84bf2a2a1d65e2ededb99e87a85
Marek Pulka <marek.pulka@gmail.com>
2015-06-30 03:03:53 +0100

    1. cucumber added 2. cobertura fixed 3. versions upgraded 4. version checker enabled

---
+++ src/test/java/com/origamicode/subdb2/CucumberRunner.java
@@ -0,0 +1,20 @@
+package com.origamicode.subdb2;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+  dryRun = false,
+  strict = true,
+/*  format = { "pretty", "html:target/cucumber" },*/
+  /*format = { "json:target_json/cucumber.json" },*/
+  plugin = {"json:target/cucumber/report.json"},
+  glue = "com.origamicode.subdb2.cucumber.steps",
+  features = "classpath:cucumber"
+)
+public class CucumberRunner {
+
+}
