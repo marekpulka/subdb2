bdabeb4606f37ec98b8ac9975b2a18f5a9b6a696
Marek Pulka <marek.pulka@gmail.com>
2019-05-28 11:21:14 +0200

    Clear unused import

--- src/test/java/com/origamicode/subdb2/cucumber/steps/CucumberSteps.java
+++ src/test/java/com/origamicode/subdb2/cucumber/steps/CucumberSteps.java
@@ -1,12 +1,11 @@
 package com.origamicode.subdb2.cucumber.steps;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 
 public class CucumberSteps {
   @Given("^I have a calculator$")
   public void i_have_a_calculator() throws Throwable {
       // Write code here that turns the phrase above into concrete actions
-      //throw new PendingException();
+      //throw new cucumber.api.PendingException();
   }
 }
